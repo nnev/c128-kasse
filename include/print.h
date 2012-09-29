5a5488ecca0d7a5b5b03880fcb90cccd197fe9c9
Michael Stapelberg <michael@stapelberg.de>
2012-09-29 16:24:48 +0200

    print_buffer needs to be larger when we print 80 characters

--- include/print.h
+++ include/print.h
@@ -6,11 +6,11 @@ void print_header();
 void log_file(const char *s);
 
 #ifdef _IS_PRINT
-char print_buffer[81];
+char print_buffer[80 + 2 + 1];
 unsigned char log_num = 0;
 int log_lines_written = 0;
 #else
-extern char print_buffer[81];
+extern char print_buffer[80 + 2 + 1];
 extern unsigned char log_num;
 extern int log_lines_written;
 
